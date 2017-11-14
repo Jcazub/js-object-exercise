8d96063e33493faabc813cc0f01e249f650b0563
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-13 19:49:57 -0500

    added bed object

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var bed = {
+  legs: 4,
+  isComfy: true,
+  yearOfPurchase: 2016,
+  name: 'Tempurpedic',
+  previousOwners: ['Bob', 'Homer', 'Fry'],
+  temperature: function(temp) {
+    console.log('The current temperature is now ' + temp + ".")
+  }
+};
